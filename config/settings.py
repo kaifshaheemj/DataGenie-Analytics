74a141344b081720106f2137cc4a814d8ef3eb02
KAIF SHAHEEM J <kaifshaheemj17@gmail.com>
2025-12-26 20:58:29 +0530

    Updated llm parameters - temp

--- config/settings.py
+++ config/settings.py
@@ -5,9 +5,9 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=1.0,
+    temperature=0.2,
     top_p=0.2, 
     max_output_tokens=2048,
-    response_format="json",
+    model_kwargs={"response_mime_type": "application/json"},
 )
 print("LLM initialized with Google Gemini-2.5-Flash model.")
